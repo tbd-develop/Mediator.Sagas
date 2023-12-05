afd09a6364c233713a69395acc24c8e1851095a4
Terry Burns-Dyson <terrybd@gmail.com>
2023-12-05 07:37:13 -0500

    Added Publishing interface for messages

---
+++ src/TbdDevelop.Mediator.Sagas/Contracts/IPublishOnComplete.cs
@@ -0,0 +1,9 @@
+﻿using Mediator;
+
+namespace TbdDevelop.Mediator.Sagas.Contracts;
+
+public interface IPublishOnComplete<out TNotification>
+    where TNotification : INotification
+{
+    TNotification Publish();
+}
