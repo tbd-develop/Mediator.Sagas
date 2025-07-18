273f998247cab2ef88e0387deb14d0c7bb5ee050
Terry Burns-Dyson <terrybd@gmail.com>
2025-07-17 22:02:12 -0400

    chore: Rename SagaConfiguration in SqlServer library
    Naming the EntityTypeConfiguration as SagaConfiguration posed a confusion when working with the code. It is indeed an entity type configuration

--- src/TbdDevelop.Mediator.Sagas.SqlServer/Configurations/SagaEntityTypeConfiguration.cs
+++ src/TbdDevelop.Mediator.Sagas.SqlServer/Configurations/SagaEntityTypeConfiguration.cs
@@ -5,18 +5,11 @@ using TbdDevelop.Mediator.Sagas.SqlServer.Models;
 
 namespace TbdDevelop.Mediator.Sagas.SqlServer.Configurations;
 
-public class SagaConfiguration : IEntityTypeConfiguration<Saga>
+public class SagaEntityTypeConfiguration(IConfiguration configuration) : IEntityTypeConfiguration<Saga>
 {
-    private readonly IConfiguration _configuration;
-
-    public SagaConfiguration(IConfiguration configuration)
-    {
-        _configuration = configuration;
-    }
-
     public void Configure(EntityTypeBuilder<Saga> builder)
     {
-        builder.ToTable(_configuration["sagas:tableName"] ?? "Sagas");
+        builder.ToTable(configuration["sagas:tableName"] ?? "Sagas");
         builder.HasKey(k => k.Id);
     }
 }
---
+++ src/TbdDevelop.Mediator.Sagas/InMemorySagaPersistence.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Concurrent;
+using TbdDevelop.Mediator.Sagas.Contracts;
+
+namespace TbdDevelop.Mediator.Sagas;
+
+/// <summary>
+/// In Memory implementation for Saga Persistence. Useful in testing or in scenarios where it
+/// doesn't matter if sagas survive an application restart.
+/// </summary>
+public class InMemorySagaPersistence : ISagaPersistence
+{
+    private readonly ConcurrentDictionary<Guid, ISaga> _sagas = new();
+
+    public Task<TSaga?> FetchSagaIfExistsByOrchestrationId<TSaga>(Guid identifier,
+        CancellationToken cancellationToken = default)
+        where TSaga : class, ISaga
+    {
+        cancellationToken.ThrowIfCancellationRequested();
+
+        if (_sagas.TryGetValue(identifier, out var saga) && saga is TSaga typedSaga)
+        {
+            return Task.FromResult<TSaga?>(typedSaga);
+        }
+
+        return Task.FromResult<TSaga?>(null);
+    }
+
+    public Task Save<T>(T saga, CancellationToken cancellationToken = default) where T : class, ISaga
+    {
+        cancellationToken.ThrowIfCancellationRequested();
+
+        ArgumentNullException.ThrowIfNull(saga);
+
+        _sagas.AddOrUpdate(saga.OrchestrationIdentifier, saga, (_, _) => saga);
+
+        return Task.CompletedTask;
+    }
+
+    public Task Delete<T>(T saga, CancellationToken cancellationToken = default) where T : class, ISaga
+    {
+        cancellationToken.ThrowIfCancellationRequested();
+
+        ArgumentNullException.ThrowIfNull(saga);
+
+        _sagas.TryRemove(saga.OrchestrationIdentifier, out _);
+
+        return Task.CompletedTask;
+    }
+}
